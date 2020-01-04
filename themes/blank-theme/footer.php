39e9fbb96a869ab931f5ec7c64caa7244ca2bc09
Tan-007 <rahi.prajapati1811@gmail.com>
2020-01-04 13:41:27 +0530

    set author name and URI

--- themes/blank-theme/footer.php
+++ themes/blank-theme/footer.php
@@ -23,17 +23,17 @@
 		<div class="cell column">
 			<span class="blank-theme-copyright-text">
 				<?php
-				$theme_uri = 'https://rtcamp.com/';
+				$theme_uri = 'https://rahicodes.wordpress.com';
 
 				/* translators: 1: Theme name, 2: Theme author. */
-				$default        = sprintf( esc_html__( '%1$s by %2$s', 'blank-theme' ), esc_html__( 'Blank Theme', 'blank-theme' ), '<a href="' . esc_url( $theme_uri ) . '" rel="designer">' . esc_html__( 'rtCamp', 'blank-theme' ) . '</a>' );
+				$default        = sprintf( esc_html__( '%1$s by %2$s', 'blank-theme' ), esc_html__( 'Blank Theme', 'blank-theme' ), '<a href="' . esc_url( $theme_uri ) . '" rel="designer">' . esc_html__( 'RahiDroid', 'blank-theme' ) . '</a>' );
 				$copyright_text = get_theme_mod( 'blank_theme_copyright_text', $default );
 
 				echo $copyright_text; /* WPCS: xss ok. */
 				?>
 			</span>
 			<span class="sep">&nbsp;|&nbsp;</span>
-			<a class="blank-theme-author-footer" href="<?php echo esc_url( 'https://rtcamp.com/' ); ?>" target="_blank" rel="noopener"><?php esc_html_e( 'Blank Theme', 'blank-theme' ); ?></a>
+			<a class="blank-theme-author-footer" href="<?php echo esc_url( 'https://rahicodes.wordpress.com/' ); ?>" target="_blank" rel="noopener"><?php esc_html_e( 'Blank Theme', 'blank-theme' ); ?></a>
 		</div>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
